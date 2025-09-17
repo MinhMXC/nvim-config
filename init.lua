955015751557ead1f565998a0c57f4a43f9cfa9e
Nguyen Quang Minh <86342515+MinhMXC@users.noreply.github.com>
2025-09-18 00:36:09 +0800

    Update

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -712,7 +712,10 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
+        jdtls = {},
         stylelint_lsp = {},
+        cssls = {},
+        html = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
