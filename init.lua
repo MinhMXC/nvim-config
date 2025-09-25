be5102bf4e3b0967c1b914f6ce703fb42c038474
Nguyen Quang Minh <86342515+MinhMXC@users.noreply.github.com>
2025-09-26 03:10:17 +0800

    Update

--- init.lua
+++ init.lua
@@ -1070,3 +1070,41 @@ vim.o.expandtab = true
 
 vim.opt.colorcolumn = '120'
 vim.api.nvim_set_hl(0, 'ColorColumn', { fg = 'none', bg = '#7A7A7A' })
+
+-- indent-blankline setup
+local highlight = {
+  'RainbowRed',
+  'RainbowYellow',
+  'RainbowBlue',
+  'RainbowOrange',
+  'RainbowGreen',
+  'RainbowViolet',
+  'RainbowCyan',
+}
+
+local hooks = require 'ibl.hooks'
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+  vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+  vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+  vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+  vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+  vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+  vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+  vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
+  vim.api.nvim_set_hl(0, 'ScopeHighlight', { fg = '#FF55FF' })
+end)
+
+require('ibl').setup {
+  debounce = 50,
+  indent = { char = '▏' },
+  scope = {
+    include = {
+      node_type = { ['*'] = { '*' } },
+    },
+    highlight = 'ScopeHighlight',
+    show_start = false,
+    show_end = false,
+  },
+}
